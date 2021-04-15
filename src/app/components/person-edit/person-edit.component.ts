2371fdf6894c3f8cab6cb18270a582672288568a
Jebati <www.den-safonov@mail.ru>
2021-04-15 19:23:17 +0300

    EmptyToNullDirective

--- src/app/components/person-edit/person-edit.component.ts
+++ src/app/components/person-edit/person-edit.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-person-edit',
   templateUrl: './person-edit.component.html',
-  styleUrls: ['./person-edit.component.scss']
+  styleUrls: ['./person-edit.component.scss'],
 })
 export class PersonEditComponent implements OnInit {
 
-  constructor() { }
+  
+  public form: FormGroup = this.fb.group({
+    shortName: [''],
+    fullName: ['', [Validators.required]],
+    address: [''],
+    tin: ['', [Validators.minLength(8), Validators.maxLength(10)]],
+    typePerson: [0],
+    passportData: ['', [Validators.minLength(10), Validators.maxLength(10)]],
+    dateBirth: [''],
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private personService: PersonService
+  ) {}
+
+  ngOnInit(): void {}
+
+  create() {
+    this.personService.createOrEditPerson(this.form.value).subscribe(() => {
+      this.router.navigate(['/persons']);
+    });
   }
-
 }
