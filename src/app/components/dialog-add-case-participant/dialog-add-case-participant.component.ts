fca6075ec823d0601df477c17184ca7e842d0e16
Jebati <www.den-safonov@mail.ru>
2021-04-17 17:25:37 +0300

    DialogAddEpisodeComponent

--- src/app/components/dialog-add-case-participant/dialog-add-case-participant.component.ts
+++ src/app/components/dialog-add-case-participant/dialog-add-case-participant.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { map, startWith, take } from 'rxjs/operators';
+import { Person } from 'src/app/entities/person.entitie';
+import { PersonService } from 'src/app/services/person.service';
 
 @Component({
   selector: 'app-dialog-add-case-participant',
@@ -8,12 +12,58 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./dialog-add-case-participant.component.scss'],
 })
 export class DialogAddCaseParticipantComponent implements OnInit {
-  public form: FormGroup = this.fb.group({});
+  public persons!: Person[];
+  public filteredPersons!: Observable<any[]>;
+
+  public form: FormGroup = this.fb.group({
+    courtCaseId: [null],
+    personId: [null, [Validators.required]],
+    role: [null],
+    placeWork: [null],
+  });
   constructor(
     public dialogRef: MatDialogRef<DialogAddCaseParticipantComponent>,
     @Inject(MAT_DIALOG_DATA) public courtCaseId: number,
-    private fb: FormBuilder
-  ) {}
+    private fb: FormBuilder,
+    private personService: PersonService
+  ) {
+    this.form.controls.courtCaseId.setValue(+this.courtCaseId);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getPersons();
+  }
+
+  getPersons() {
+    this.personService
+      .getPersons()
+      .pipe(take(1))
+      .subscribe((ps) => {
+        this.persons = ps.filter((p) => !p.isLegal);
+        this.filteredPersons = this.form.controls.personId.valueChanges.pipe(
+          startWith(''),
+          map((value) => this._filter(value))
+        );
+      });
+  }
+
+  private _filter(value: string | number): Person[] {
+    if (typeof value === 'string') {
+      const filterValue = value?.toLowerCase();
+      return this.persons.filter((person) =>
+        person.fullName.toLowerCase().includes(filterValue)
+      );
+    } else {
+      const person = this.persons.find((p) => p.personId === value);
+      return person ? [person] : [];
+    }
+  }
+
+  displayFn(personId: number): string {
+    return this.persons?.find((p) => p.personId === personId)?.fullName || '';
+  }
+
+  create() {
+    console.log(this.form.value);
+  }
 }
--- src/app/components/dialog-add-episode/dialog-add-episode.component.ts
+++ src/app/components/dialog-add-episode/dialog-add-episode.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
@@ -8,12 +8,30 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./dialog-add-episode.component.scss'],
 })
 export class DialogAddEpisodeComponent implements OnInit {
-  public form: FormGroup = this.fb.group({});
+  public form: FormGroup = this.fb.group({
+    courtCaseId: [null],
+    workingTitle: [null, [Validators.required]],
+    categoryCrime: [null, [Validators.required]],
+    typeCrime: [null, [Validators.required]],
+    dateStartCrime: [null, [Validators.required]],
+    dateEndCrime: [null, [Validators.required]],
+    placeCrime: [null, [Validators.required]],
+    motiveCrime: [null],
+    purposeCrime: [null],
+    toolCrime: [null],
+    notes: [null],
+  });
   constructor(
     public dialogRef: MatDialogRef<DialogAddEpisodeComponent>,
     @Inject(MAT_DIALOG_DATA) public courtCaseId: number,
     private fb: FormBuilder
-  ) {}
+  ) {
+    this.form.controls.courtCaseId.setValue(+this.courtCaseId);
+  }
 
   ngOnInit(): void {}
+
+  create() {
+    console.log(this.form.value);
+  }
 }
